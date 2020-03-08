f5026db48f8c2b63797db573c5fc372d5fceacb4
John Hillert <john.hillert@gmail.com>
2020-03-08 14:14:20 -0500

    correctly absolute

--- src/components/bootstrap-modal.jsx
+++ src/components/bootstrap-modal.jsx
@@ -3,27 +3,47 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import styled from 'styled-components';
 
+/*
+  Need to study "portals"
+  Setting container to 'fixed' gives elements inside an anchor.
+    absolute:  Element is positioned relative to its first positioned
+                (not static) ancestor element
+    fixed:     Element is positioned relative to browser window.
 
-const S = {}
+    MODAL RENDERED
+    Below are default values for ReactBootstrap modal. These steps set
+    the modal.
 
-// fixing this gives the modal a fram of reference.
-S.ModalContainer = styled.div`
-    position: fixed;
-    bottom: 0px;
-    right: 0px;
-`;
+      1. Find the first element of the modal container that is set at 'fixed'.
+         If one does not exist, make one.
+      2. Set the child of the fixed element relative to the position of the parent.
+      3. Find the position of the relevant edge of the container. The relevant edge
+         is the one you are offsetting from via "absolute" using Left, Right, Top or
+         Bottom.
+      4. Set the child for absolute.  It can now be moved relative to the fixed
+         parent container edges.  Calculate the necessary deltas to move the modal
+         where you want it to go.
 
+      <div class="modal-backdrop show"></div>       position: fixed; (default)
+      <div class="modal" style="display: block;">   position: fixed; (default)
+        <div class="modal-dialog">                  position: relative (default)
+          <div class="modal-content">
+            <div class="modal-header">Header</div>
+            <div class="modal-body">Click Me</div>
+            <div class="modal-footer">Footer</div>
+          </div>
+        </div>
+      </div>`
+*/
 
-bootstrap modal
-
-S.Modal = styled(Modal)`
-  .modal-backdrop.show {
-  }
+const SModal = styled(Modal)`
+  .modal-backdrop.show { /* set at fixed by default */ }
+  .modal { /* set at fixed by default */ }
 
   .modal-dialog {
+    bottom: 185px;
+    right: 170px;
     position: absolute;
-    bottom: 0px;
-    right: 0px;
   }
 
   .modal-content {
@@ -33,11 +53,12 @@ S.Modal = styled(Modal)`
   }
 `;
 
-S.ModalBody = styled(S.Modal.Body)``;
+const SModalBody = styled(Modal.Body)``;
 
+const SModalHeader = styled(Modal.Body)``;
 
-S.Button = styled(Button)`
-  background-color: red;
+const FAB = styled(Button)`
+  background-color: green;
   border-radius: 50%;
   border: none;
   height:3rem;
@@ -53,25 +74,24 @@ const BootModal = ({ handleClick }) => {
 
   return (
     <>
-      <S.ModalContainer>
-        <S.Modal
+      <div className='my-container-class'>
+        <SModal
           animation={false}
           show={true}
           keyboard
         >
-          <S.Modal.Header>Header</S.Modal.Header>
-          <S.ModalBody
-            as='button'
+          <SModalHeader>Header</SModalHeader>
+          <SModalBody
             onClick={() => setToggle(false)}
           >
             Click Me
-          </S.ModalBody>
-          <S.Modal.Footer>Footer</S.Modal.Footer>
-        </S.Modal>
-      </S.ModalContainer>
-      <S.Button
+          </SModalBody>
+          <Modal.Footer>Footer</Modal.Footer>
+        </SModal>
+      </div>
+      <FAB
         onClick={() => setToggle(() => !toggle)}
-      ></S.Button>
+      ></FAB>
     </>
   );
 }
